825d2e6e525b716d241eb8d17f5e0842fd8dab59
devopscourses <skydevapp@gmail.com>
2019-05-06 03:11:59 -0400

    terraform-1_7

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -1,9 +1,3 @@
-provider "google" {
-  version = "2.0.0"
-  project = "${var.project}"
-  region  = "${var.region}"
-}
-
 module "storage-bucket" {
   source  = "SweetOps/storage-bucket/google"
   version = "0.1.1"
